fc63f9d221c00456c8c92851f56d662acae041b2
gopih <gopi47590@gmail.com>
2025-04-12 12:57:21 +0530

    security group

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+
+}
+
+variable "environment" {
+
+}
+
+variable "sg_name" {
+
+}
+
+variable "sg_descrption" {
+
+}
+
+variable "common_tags" {
+    type = map
+}
+
+variable "sg_tags" {
+    default = {}
+}
